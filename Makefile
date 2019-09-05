117e6583739abaf05f4cd612399b8f10558eaf4c
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2019-09-05 23:55:29 +0200

    Adds support for datasource plugin to call back into Grafana

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ all: clean golang
 
 clean:
 	rm -rf go/datasource/*.pb.go
+	rm -rf go/renderer/*.pb.go
 
 golang:
 	protoc -I . datasource.proto --go_out=plugins=grpc:go/datasource/.
--- go/datasource/service.go
+++ go/datasource/service.go
@@ -3,40 +3,94 @@ package datasource
 import (
 	"context"
 
-	plugin "github.com/hashicorp/go-plugin"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
 )
 
-type DatasourcePlugin interface {
-	Query(ctx context.Context, req *DatasourceRequest) (*DatasourceResponse, error)
+// GrafanaAPI is the Grafana API interface that allows a datasource plugin to callback and request additional information from Grafana.
+type GrafanaAPI interface {
+	QueryDatasource(ctx context.Context, req *QueryDatasourceRequest) (*QueryDatasourceResponse, error)
 }
 
+// DatasourcePlugin is the Grafana datasource interface.
+type DatasourcePlugin interface {
+	Query(ctx context.Context, req *DatasourceRequest, api GrafanaAPI) (*DatasourceResponse, error)
+}
+
+// DatasourcePluginImpl is the implementation of plugin.Plugin so that it can be served/consumed as a Grafana datasource plugin.
+// In addition, it also implements plugin.GRPCPlugin so that the it can be served over gRPC.
 type DatasourcePluginImpl struct {
 	plugin.NetRPCUnsupportedPlugin
-	Plugin DatasourcePlugin
+	Impl DatasourcePlugin
 }
 
 func (p *DatasourcePluginImpl) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
-	RegisterDatasourcePluginServer(s, &GRPCServer{p.Plugin})
+	RegisterDatasourcePluginServer(s, &GRPCServer{
+		broker: broker,
+		Impl:   p.Impl,
+	})
 	return nil
 }
 
 func (p *DatasourcePluginImpl) GRPCClient(ctx context.Context, broker *plugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
-	return &GRPCClient{NewDatasourcePluginClient(c)}, nil
+	return &GRPCClient{
+		broker: broker,
+		client: NewDatasourcePluginClient(c),
+	}, nil
 }
 
+var _ plugin.GRPCPlugin = &DatasourcePluginImpl{}
+
+// GRPCClient is an implementation of DatasourcePluginClient that talks over RPC.
 type GRPCClient struct {
-	DatasourcePluginClient
+	broker *plugin.GRPCBroker
+	client DatasourcePluginClient
 }
 
-func (m *GRPCClient) Query(ctx context.Context, req *DatasourceRequest) (*DatasourceResponse, error) {
-	return m.DatasourcePluginClient.Query(ctx, req)
+func (m *GRPCClient) Query(ctx context.Context, req *DatasourceRequest, api GrafanaAPI) (*DatasourceResponse, error) {
+	var s *grpc.Server
+	serverFunc := func(opts []grpc.ServerOption) *grpc.Server {
+		s = grpc.NewServer(opts...)
+		RegisterGrafanaAPIServer(s, api)
+
+		return s
+	}
+
+	brokerID := m.broker.NextId()
+	go m.broker.AcceptAndServe(brokerID, serverFunc)
+
+	res, err := m.client.Query(ctx, req)
+
+	s.Stop()
+	return res, err
 }
 
+// GRPCServer is the gRPC server that GRPCClient talks to.
 type GRPCServer struct {
-	DatasourcePlugin
+	broker *plugin.GRPCBroker
+	Impl   DatasourcePlugin
 }
 
 func (m *GRPCServer) Query(ctx context.Context, req *DatasourceRequest) (*DatasourceResponse, error) {
-	return m.DatasourcePlugin.Query(ctx, req)
+	conn, err := m.broker.Dial(req.RequestId)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	api := &GRPCGrafanaAPIClient{NewGrafanaAPIClient(conn)}
+	return m.Impl.Query(ctx, req, api)
+}
+
+// GRPCGrafanaAPIClient is an implementation of GrafanaAPIClient that talks over RPC.
+type GRPCGrafanaAPIClient struct{ client GrafanaAPIClient }
+
+func (m *GRPCGrafanaAPIClient) QueryDatasource(ctx context.Context, req *QueryDatasourceRequest) (*QueryDatasourceResponse, error) {
+	resp, err := m.client.QueryDatasource(ctx, req)
+	if err != nil {
+		hclog.Default().Info("grafana.QueryDatasource", "client", "start", "err", err)
+		return nil, err
+	}
+	return resp, err
 }
